ed3eef9b090f5a59a1d05bfc020b63ec1ccce2b8
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:44:48 +0800

    Create DataServerType.java

---
+++ src/main/java/com/neusoft/crm/entity/DataServerType.java
@@ -0,0 +1,20 @@
+package com.neusoft.crm.entity;
+
+public class DataServerType {
+	private int dstId;
+	private String dstName;
+	
+	public int getDstId() {
+		return dstId;
+	}
+	public void setDstId(int dstId) {
+		this.dstId = dstId;
+	}
+	public String getDstName() {
+		return dstName;
+	}
+	public void setDstName(String dstName) {
+		this.dstName = dstName;
+	}
+
+}
