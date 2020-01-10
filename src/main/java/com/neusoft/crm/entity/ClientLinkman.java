23f9b2989f791452d8a5c3c95ec563c7db2a412c
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:23:19 +0800

    Create ClientLinkman.java

---
+++ src/main/java/com/neusoft/crm/entity/ClientLinkman.java
@@ -0,0 +1,62 @@
+package com.neusoft.crm.entity;
+
+public class ClientLinkman {
+	private int clientLinkmanId;
+	private String clientLinkmanName;
+	private int clientLinkmanSex;
+	private String clientLinkmanJob;
+	private String clientLinkmanTel;
+	private String clientLinkmanMobile;
+	private String remark;
+	private String clientCode;
+	
+	public int getClientLinkmanId() {
+		return clientLinkmanId;
+	}
+	public void setClientLinkmanId(int clientLinkmanId) {
+		this.clientLinkmanId = clientLinkmanId;
+	}
+	public String getClientLinkmanName() {
+		return clientLinkmanName;
+	}
+	public void setClientLinkmanName(String clientLinkmanName) {
+		this.clientLinkmanName = clientLinkmanName;
+	}
+	public int getClientLinkmanSex() {
+		return clientLinkmanSex;
+	}
+	public void setClientLinkmanSex(int clientLinkmanSex) {
+		this.clientLinkmanSex = clientLinkmanSex;
+	}
+	public String getClientLinkmanJob() {
+		return clientLinkmanJob;
+	}
+	public void setClientLinkmanJob(String clientLinkmanJob) {
+		this.clientLinkmanJob = clientLinkmanJob;
+	}
+	public String getClientLinkmanTel() {
+		return clientLinkmanTel;
+	}
+	public void setClientLinkmanTel(String clientLinkmanTel) {
+		this.clientLinkmanTel = clientLinkmanTel;
+	}
+	public String getClientLinkmanMobile() {
+		return clientLinkmanMobile;
+	}
+	public void setClientLinkmanMobile(String clientLinkmanMobile) {
+		this.clientLinkmanMobile = clientLinkmanMobile;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public String getClientCode() {
+		return clientCode;
+	}
+	public void setClientCode(String clientCode) {
+		this.clientCode = clientCode;
+	}
+
+}
