9102744b68051193048007e9b7683952136fd5f8
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:44:40 +0800

    Create ClientServer.java

---
+++ src/main/java/com/neusoft/crm/entity/ClientServer.java
@@ -0,0 +1,105 @@
+package com.neusoft.crm.entity;
+
+public class ClientServer {
+	private int serverId;
+	private int serverTypeId;
+	private String serverTitle;
+	private String clientCode;
+	private int serverStatus;
+	private String serverContent;
+	private int serverCreateId;
+	private String serverCreateDate;
+	private int serverDueId;
+	private String serverDueDate;
+	private String serverProcessing;
+	private String processingDate;
+	private String processingResult;
+	private int clientContentment;
+	
+	public int getServerId() {
+		return serverId;
+	}
+	public void setServerId(int serverId) {
+		this.serverId = serverId;
+	}
+	public int getServerTypeId() {
+		return serverTypeId;
+	}
+	public void setServerTypeId(int serverTypeId) {
+		this.serverTypeId = serverTypeId;
+	}
+	public String getServerTitle() {
+		return serverTitle;
+	}
+	public void setServerTitle(String serverTitle) {
+		this.serverTitle = serverTitle;
+	}
+	public String getClientCode() {
+		return clientCode;
+	}
+	public void setClientCode(String clientCode) {
+		this.clientCode = clientCode;
+	}
+	public int getServerStatus() {
+		return serverStatus;
+	}
+	public void setServerStatus(int serverStatus) {
+		this.serverStatus = serverStatus;
+	}
+	public String getServerContent() {
+		return serverContent;
+	}
+	public void setServerContent(String serverContent) {
+		this.serverContent = serverContent;
+	}
+	public int getServerCreateId() {
+		return serverCreateId;
+	}
+	public void setServerCreateId(int serverCreateId) {
+		this.serverCreateId = serverCreateId;
+	}
+	public String getServerCreateDate() {
+		return serverCreateDate;
+	}
+	public void setServerCreateDate(String serverCreateDate) {
+		this.serverCreateDate = serverCreateDate;
+	}
+	public int getServerDueId() {
+		return serverDueId;
+	}
+	public void setServerDueId(int serverDueId) {
+		this.serverDueId = serverDueId;
+	}
+	public String getServerDueDate() {
+		return serverDueDate;
+	}
+	public void setServerDueDate(String serverDueDate) {
+		this.serverDueDate = serverDueDate;
+	}
+	public String getServerProcessing() {
+		return serverProcessing;
+	}
+	public void setServerProcessing(String serverProcessing) {
+		this.serverProcessing = serverProcessing;
+	}
+	public String getProcessingDate() {
+		return processingDate;
+	}
+	public void setProcessingDate(String processingDate) {
+		this.processingDate = processingDate;
+	}
+	public String getProcessingResult() {
+		return processingResult;
+	}
+	public void setProcessingResult(String processingResult) {
+		this.processingResult = processingResult;
+	}
+	public int getClientContentment() {
+		return clientContentment;
+	}
+	public void setClientContentment(int clientContentment) {
+		this.clientContentment = clientContentment;
+	}
+	
+
+}
