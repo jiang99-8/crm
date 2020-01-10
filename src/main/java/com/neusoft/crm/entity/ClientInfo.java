ab7215e6bce6ab237b775ae248989f37db59d5dc
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:23:15 +0800

    Create ClientInfo.java

---
+++ src/main/java/com/neusoft/crm/entity/ClientInfo.java
@@ -0,0 +1,168 @@
+package com.neusoft.crm.entity;
+
+public class ClientInfo {
+	private String clientCode;
+	private String clientName;
+	private int clientAreaId;
+	private int clientCustId;
+	private int clientLevelId;
+	private int clientContentment;
+	private int clientCredit;
+	private String clientAddress;
+	private String clientDakCode;
+	private String clientTel;
+	private String clientFax;
+	private String clientNet;
+	private String clientCharter;
+	private String clientCorporation;
+	private int clientBankroll;
+	private int clientTurnoverYear;
+	private String clientBank;
+	private String clientAccounts;
+	private String clientNativeTax;	
+	private String clientCountryTax;
+	private int clientState;
+	private String clientLoseReason;
+	private String clientLoseDate;
+	
+	public String getClientCode() {
+		return clientCode;
+	}
+	public void setClientCode(String clientCode) {
+		this.clientCode = clientCode;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public int getClientAreaId() {
+		return clientAreaId;
+	}
+	public void setClientAreaId(int clientAreaId) {
+		this.clientAreaId = clientAreaId;
+	}
+	public int getClientCustId() {
+		return clientCustId;
+	}
+	public void setClientCustId(int clientCustId) {
+		this.clientCustId = clientCustId;
+	}
+	public int getClientLevelId() {
+		return clientLevelId;
+	}
+	public void setClientLevelId(int clientLevelId) {
+		this.clientLevelId = clientLevelId;
+	}
+	public int getClientContentment() {
+		return clientContentment;
+	}
+	public void setClientContentment(int clientContentment) {
+		this.clientContentment = clientContentment;
+	}
+	public int getClientCredit() {
+		return clientCredit;
+	}
+	public void setClientCredit(int clientCredit) {
+		this.clientCredit = clientCredit;
+	}
+	public String getClientAddress() {
+		return clientAddress;
+	}
+	public void setClientAddress(String clientAddress) {
+		this.clientAddress = clientAddress;
+	}
+	public String getClientDakCode() {
+		return clientDakCode;
+	}
+	public void setClientDakCode(String clientDakCode) {
+		this.clientDakCode = clientDakCode;
+	}
+	public String getClientTel() {
+		return clientTel;
+	}
+	public void setClientTel(String clientTel) {
+		this.clientTel = clientTel;
+	}
+	public String getClientFax() {
+		return clientFax;
+	}
+	public void setClientFax(String clientFax) {
+		this.clientFax = clientFax;
+	}
+	public String getClientNet() {
+		return clientNet;
+	}
+	public void setClientNet(String clientNet) {
+		this.clientNet = clientNet;
+	}
+	public String getClientCharter() {
+		return clientCharter;
+	}
+	public void setClientCharter(String clientCharter) {
+		this.clientCharter = clientCharter;
+	}
+	public String getClientCorporation() {
+		return clientCorporation;
+	}
+	public void setClientCorporation(String clientCorporation) {
+		this.clientCorporation = clientCorporation;
+	}
+	public int getClientBankroll() {
+		return clientBankroll;
+	}
+	public void setClientBankroll(int clientBankroll) {
+		this.clientBankroll = clientBankroll;
+	}
+	public int getClientTurnoverYear() {
+		return clientTurnoverYear;
+	}
+	public void setClientTurnoverYear(int clientTurnoverYear) {
+		this.clientTurnoverYear = clientTurnoverYear;
+	}
+	public String getClientBank() {
+		return clientBank;
+	}
+	public void setClientBank(String clientBank) {
+		this.clientBank = clientBank;
+	}
+	public String getClientAccounts() {
+		return clientAccounts;
+	}
+	public void setClientAccounts(String clientAccounts) {
+		this.clientAccounts = clientAccounts;
+	}
+	public String getClientNativeTax() {
+		return clientNativeTax;
+	}
+	public void setClientNativeTax(String clientNativeTax) {
+		this.clientNativeTax = clientNativeTax;
+	}
+	public String getClientCountryTax() {
+		return clientCountryTax;
+	}
+	public void setClientCountryTax(String clientCountryTax) {
+		this.clientCountryTax = clientCountryTax;
+	}
+	public int getClientState() {
+		return clientState;
+	}
+	public void setClientState(int clientState) {
+		this.clientState = clientState;
+	}
+	public String getClientLoseReason() {
+		return clientLoseReason;
+	}
+	public void setClientLoseReason(String clientLoseReason) {
+		this.clientLoseReason = clientLoseReason;
+	}
+	public String getClientLoseDate() {
+		return clientLoseDate;
+	}
+	public void setClientLoseDate(String clientLoseDate) {
+		this.clientLoseDate = clientLoseDate;
+	}
+	
+
+}
