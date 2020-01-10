b069e40503549794e76b2ecb0e437f526d555d02
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:44:43 +0800

    Create DataArea.java

---
+++ src/main/java/com/neusoft/crm/entity/DataArea.java
@@ -0,0 +1,20 @@
+package com.neusoft.crm.entity;
+
+public class DataArea {
+	private int daId;
+	private String daName;
+	
+	public int getDaId() {
+		return daId;
+	}
+	public void setDaId(int daId) {
+		this.daId = daId;
+	}
+	public String getDaName() {
+		return daName;
+	}
+	public void setDaName(String daName) {
+		this.daName = daName;
+	}
+
+}
