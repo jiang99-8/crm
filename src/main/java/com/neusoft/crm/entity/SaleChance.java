bd32a12cb880dcb53e26fb9b2aa4073d8644f624
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:23:22 +0800

    Create SaleChance.java

---
+++ src/main/java/com/neusoft/crm/entity/SaleChance.java
@@ -0,0 +1,97 @@
+package com.neusoft.crm.entity;
+
+public class SaleChance {
+	private int chanceId;
+	private String chanceSource;
+	private String chanceCustName;
+	private String chanceTitle;
+	private int chanceRate;
+	private String chanceLinkman;
+	private String chanceTel;
+	private String chanceDesc;
+	private int chanceCreateId;
+	private String chanceCreateDate;
+	private int chanceDueId;
+	private String chanceDueDate;
+	private int chanceStatus;
+	
+	public int getChanceId() {
+		return chanceId;
+	}
+	public void setChanceId(int chanceId) {
+		this.chanceId = chanceId;
+	}
+	public String getChanceSource() {
+		return chanceSource;
+	}
+	public void setChanceSource(String chanceSource) {
+		this.chanceSource = chanceSource;
+	}
+	public String getChanceCustName() {
+		return chanceCustName;
+	}
+	public void setChanceCustName(String chanceCustName) {
+		this.chanceCustName = chanceCustName;
+	}
+	public String getChanceTitle() {
+		return chanceTitle;
+	}
+	public void setChanceTitle(String chanceTitle) {
+		this.chanceTitle = chanceTitle;
+	}
+	public int getChanceRate() {
+		return chanceRate;
+	}
+	public void setChanceRate(int chanceRate) {
+		this.chanceRate = chanceRate;
+	}
+	public String getChanceLinkman() {
+		return chanceLinkman;
+	}
+	public void setChanceLinkman(String chanceLinkman) {
+		this.chanceLinkman = chanceLinkman;
+	}
+	public String getChanceTel() {
+		return chanceTel;
+	}
+	public void setChanceTel(String chanceTel) {
+		this.chanceTel = chanceTel;
+	}
+	public String getChanceDesc() {
+		return chanceDesc;
+	}
+	public void setChanceDesc(String chanceDesc) {
+		this.chanceDesc = chanceDesc;
+	}
+	public int getChanceCreateId() {
+		return chanceCreateId;
+	}
+	public void setChanceCreateId(int chanceCreateId) {
+		this.chanceCreateId = chanceCreateId;
+	}
+	public String getChanceCreateDate() {
+		return chanceCreateDate;
+	}
+	public void setChanceCreateDate(String chanceCreateDate) {
+		this.chanceCreateDate = chanceCreateDate;
+	}
+	public int getChanceDueId() {
+		return chanceDueId;
+	}
+	public void setChanceDueId(int chanceDueId) {
+		this.chanceDueId = chanceDueId;
+	}
+	public String getChanceDueDate() {
+		return chanceDueDate;
+	}
+	public void setChanceDueDate(String chanceDueDate) {
+		this.chanceDueDate = chanceDueDate;
+	}
+	public int getChanceStatus() {
+		return chanceStatus;
+	}
+	public void setChanceStatus(int chanceStatus) {
+		this.chanceStatus = chanceStatus;
+	}
+
+}
