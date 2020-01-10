c7c77aa5c85e816c6a3299760c1e05f341dbaee4
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-10 21:44:45 +0800

    Create DataClientLevel.java

---
+++ src/main/java/com/neusoft/crm/entity/DataClientLevel.java
@@ -0,0 +1,21 @@
+package com.neusoft.crm.entity;
+
+public class DataClientLevel {
+	private int dclId;
+	private String dclName;
+	
+	public int getDclId() {
+		return dclId;
+	}
+	public void setDclId(int dclId) {
+		this.dclId = dclId;
+	}
+	public String getDclName() {
+		return dclName;
+	}
+	public void setDclName(String dclName) {
+		this.dclName = dclName;
+	}
+	
+
+}
