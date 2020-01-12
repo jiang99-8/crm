6602b2d21456927575251cda08b20b9ef97fb552
jiang <59642398+jiang99-8@users.noreply.github.com>
2020-01-12 16:13:21 +0800

    Update CrmApplication.java

--- src/main/java/com/neusoft/crm/CrmApplication.java
+++ src/main/java/com/neusoft/crm/CrmApplication.java
@@ -2,7 +2,9 @@ package com.neusoft.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class CrmApplication {
 
